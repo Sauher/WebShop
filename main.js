4507cf3feac87799866a0fc163cb12665b8753cf
Acsbeni <acsbenjamin@turr.hu>
2024-12-02 08:40:43 +0100

    WEB-6 Price árak változása a slider által

--- main.js
+++ main.js
@@ -1,25 +1,37 @@
+//slider js
+
 const range1 = document.getElementById("range1");
 const range2 = document.getElementById("range2");
 const sliderTrack = document.querySelector(".slider-track");
 const minGap = 1;
 const sliderMaxValue = 100;
 
+const minar = document.getElementById("minar");
+const maxar = document.getElementById("maxar");
+
 function updateSlider() {
   const range1Value = parseInt(range1.value);
   const range2Value = parseInt(range2.value);
+
+  const minarValue = parseInt(minar.value)
+  const maxarValue = parseInt(maxar.value)
 
  
   if (range2Value - range1Value < minGap) {
     if (this.id === "range1") {
       range1.value = range2Value - minGap;
+      
     } else {
       range2.value = range1Value + minGap;
     }
   }
 
   
-  sliderTrack.style.left = `${(range1.value / sliderMaxValue) * 100}%`;
+  sliderTrack.style.left = `${((range1.value) / sliderMaxValue) * 100}%`;
   sliderTrack.style.width = `${((range2.value - range1.value) / sliderMaxValue) * 100}%`;
+  minar.innerHTML = `${range1.value}$`;
+  maxar.innerHTML = `${range2.value}$`;
+
 }
 
 
@@ -33,3 +45,8 @@ const button = document.getElementsByClassName(".button");
 function Filter(){
   console.log("A gomb meg lett nyomva")
 }
+
+
+
+
+
