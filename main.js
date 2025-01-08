701a30547cd2eec013c85032f47bd8c24846b731
Sauher <schaueroliver@turr.hu>
2025-01-08 08:40:07 +0100

    WEB-7 Burger menü dropdown és ikonok beillesztése

--- main.js
+++ main.js
@@ -47,10 +47,16 @@ function Filter(){
 }
 function myFunction() {
   let x = document.getElementById("myLinks");
+  let ikon1 = document.getElementById("menuikon1");
+  let ikon2 = document.getElementById("menuikon2");
   if (x.style.display === "block") {
     x.style.display = "none";
+    ikon1.style.display = "block";
+    ikon2.style.display = "none";
   } else {
     x.style.display = "block";
+    ikon1.style.display = "none";
+    ikon2.style.display = "block";
   }
 }
 
