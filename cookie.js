be334792c90c85ed43bb7445f7b3b95ff93b8fc3
Sauher <schaueroliver@turr.hu>
2025-03-26 08:38:54 +0100

    WEB-30 Cookie pop up létrehozása

---
+++ cookie.js
@@ -0,0 +1,8 @@
+alertbox.render({
+    title: 'Cookiek',
+    message: "Elfogadja a cookiekat? (Nincs más opció)",
+    btnTitle: 'Elfogadás',
+    themeColor: '#eb5e5f',
+    btnColor: '#7CFC00',
+    btnColor: true
+  });
